8bbea0a910d184347dabbbee3e8afc9a752ad3c0
AKANKSH-GUPTHA <akankshmodadugu12345@gmail.com>
2026-03-19 16:33:48 +0530

    connected frontend to deployed backend

--- frontend/src/context/StoreContext.js
+++ frontend/src/context/StoreContext.js
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null)
 const StoreContextProvider = ({ children }) => {
   const [cartItems, setCartItems] = useState({})
   const [token, setToken] = useState(localStorage.getItem('token') || '')
-  const url = 'http://localhost:4000'
+  const url = 'https://food-delivery-app-s8rh.onrender.com'
 
   const addToCart = (itemId) => {
     setCartItems(prev => ({
